b9f12f68b7d2def743a15b14618f7d780fd825b4
Siddhesh Desai <siddheshdesai777@gmail.com>
2025-02-01 21:09:17 +0530

    view itinerory backend done

--- web/routes/api.js
+++ web/routes/api.js
@@ -5,19 +5,7 @@ const userController = require("../controllers/user");
 const { requireUserAuth } = require("../middleware/auth");
 
 router.post('/register', userController.register);
-router.post('/login', userController.login);
-router.post('/account', requireUserAuth, userController.addAccount);
-router.get('/logout', requireUserAuth, userController.logout);
-router.post('/botresponse', requireUserAuth, userController.getResponse);
-router.post('/investment', requireUserAuth, userController.addInvestment);
-router.post('/goal', requireUserAuth, userController.addGoal);
-router.post('/expense', requireUserAuth, userController.addExpense);
-router.post('/liabilities', requireUserAuth, userController.addLiabilities);
-router.post("/family", requireUserAuth, userController.addFamily);
-router.post('/family/join', requireUserAuth, userController.joinFamily);
-router.get('/family/delete/:familyId/:goalId', requireUserAuth, userController.deleteFamilyGoal);
-router.post('/family/goal/:familyId', requireUserAuth, userController.addFamilyGoal);
-router.get('/goal/delete/:goalType', requireUserAuth, userController.deleteGoal);
+router.post('/login', userController.login)
 router.get('/itinerary/:itineraryid', userController.renderItinerary);
 
 
--- web/routes/route.js
+++ web/routes/route.js
@@ -8,5 +8,5 @@ router.get("/", requireUserAuth, userController.renderDashboard);
 
 router.get("/login", userController.renderLogin);
 router.get("/register", userController.renderRegister);
-
+router.get("/itinerary/:itineraryid", requireUserAuth, userController.renderItinerary);
 module.exports = router;
