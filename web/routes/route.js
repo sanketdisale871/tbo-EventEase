06f9179db520a28c9a5d109181fa4c286e0d40bd
Sanket <sanketdisale871@gmail.com>
2025-02-01 20:13:31 +0530

    Login/Sign Up Done

--- web/routes/route.js
+++ web/routes/route.js
@@ -5,13 +5,6 @@ const userController = require("../controllers/user");
 const { requireUserAuth } = require("../middleware/auth");
 
 router.get("/", requireUserAuth, userController.renderDashboard);
-router.get("/investments", requireUserAuth, userController.renderInvestment);
-router.get("/goals", requireUserAuth, userController.renderGoals);
-router.get("/learnings", requireUserAuth, userController.renderLearnings);
-router.get("/family", requireUserAuth, userController.renderFamily);
-router.get("/citigpt", requireUserAuth, userController.renderChat);
-router.get("/expenses", requireUserAuth, userController.renderExpenses);
-router.get("/liabilities", requireUserAuth, userController.renderLiabilities);
 
 router.get("/login", userController.renderLogin);
 router.get("/register", userController.renderRegister);
