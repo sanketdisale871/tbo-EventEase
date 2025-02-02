e466d4d4a8a40ecaf77d6f58ff6d7c9322b48402
Siddhesh Desai <siddheshdesai777@gmail.com>
2025-02-02 15:59:17 +0530

    Publish event done

--- web/routes/route.js
+++ web/routes/route.js
@@ -9,10 +9,13 @@ router.get("/", requireUserAuth, userController.renderDashboard);
 router.get("/login", userController.renderLogin);
 router.get("/register", userController.renderRegister);
 router.get("/citigpt", requireUserAuth, userController.renderChat);
+router.post("/publish-event", requireUserAuth, userController.publishEvent);
+router.get("/event/:eventid", requireUserAuth, userController.renderEvent);
 
 router.get(
   "/itinerary/:itineraryid",
   requireUserAuth,
   userController.renderItinerary
 );
+
 module.exports = router;
